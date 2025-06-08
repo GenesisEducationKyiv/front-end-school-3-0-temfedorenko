bb13d39291712c86c3b3558168f124b6e4448c1f
Artem Fedorenko <temfedorenko@gmail.com>
2025-06-08 13:00:44 +0300

    HW-3/Url-filters-with-tsbelt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 
@@ -9,14 +10,14 @@ import { AppHeader } from './components/AppHeader';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <AppHeader />
       <Box component='main' flexGrow={1}><TracksPage /></Box>
       <Box component='footer'>
         <Typography mt='50px' align='center' variant='h6'>Developed by Artem Fedorenko | 2025</Typography>
       </Box>
       <ToastContainer autoClose={3000} position='top-left' />
-    </>
+    </BrowserRouter>
   );
 }
 
